ebfc65cef8e06deb0fcd3ecdf9438839cd8aba1d
mrtwumgh <twumstephen2006@gmail.com>
2023-11-21 05:37:53 +0000

    add: 0-bubble_sort.c and 0-O added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using the bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: Void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, flag;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+}
